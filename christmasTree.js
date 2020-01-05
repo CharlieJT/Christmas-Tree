5accd30901d0aa66456f0f281ed9df9c46d62c5a
EC2 Default User <ec2-user@ip-172-31-46-228.ec2.internal>
2020-01-05 22:03:29 +0000

    Christmas tree coding challange

--- christmasTree.js
+++ christmasTree.js
@@ -6,13 +6,11 @@ const christmasTree = () => {
       Edit "heightOfBlocksInTree" to change how high each of the blocks are in the christmas tree
     */
     
-    // vvvvvvvv
+
     
-    const blocksInTree = 6;
+    const blocksInTree = 6; // Change how many blocks there are in the tree
     
-    const heightOfBlocksInTree = 3;
-  
-    // ^^^^^^^^
+    const heightOfBlocksInTree = 3; // Change the height of the blocks in the tree
   
   
   
@@ -28,11 +26,17 @@ const christmasTree = () => {
     For loop to create the building blocks for the tree with spacing inside of the tree 
     */
     for (let i = 0; i < heightOfTree; i++) {
-      spacesInsideOfTree.push("\xa0\xa0");
+      if (i === 0) {
+        treeBuildingBlocks.push("\xa0|\xa0");
+      } else if (i === 1) {
+        spacesInsideOfTree.push("\xa0");
+      } else {
+        spacesInsideOfTree.push("\xa0\xa0");
+      }
       spacesToString = spacesInsideOfTree.join("");
       let treeBuild = `/${spacesToString}\\`;
       if(i === 0) {
-        treeBuildingBlocks.push("\xa0\xa0|\xa0\xa0\xa0");
+        
       } else if ((i + 1) === heightOfTree) {
         lengthOfTreeBuild.push(treeBuild);
         treeBuildingBlocks.push(treeBuild);
@@ -70,16 +74,20 @@ const christmasTree = () => {
     const bottomOfTreeToString = bottomOfTree.join("");
     console.log(`|${bottomOfTreeToString}|`);
   
-     /* For loop for creating the base of the tree */
+    /* For loop for creating the base of the tree */
     for (let i = 0; i < blocksInTree; i++) {
       for (let j = 0; j < heightOfBlocksInTree; j++) {
         spacesForBaseOfTree.push("\xa0");
       }
       spacesForBaseOfTree.pop();
     }
+    /* Final for loop for the removing the remaining spaces in the tree */
+    for (let i = 0; i < 2; i++) {
+      spacesForBaseOfTree.pop();
+    }
   
     const spacesForBaseOfTreeString = spacesForBaseOfTree.join("");
-    console.log(`${spacesForBaseOfTreeString}\\______/${spacesForBaseOfTreeString}`);
+    console.log(`${spacesForBaseOfTreeString}\\_______/${spacesForBaseOfTreeString}`);
   }
                 
   christmasTree();
